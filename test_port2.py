50cf80b735a4279bc9802ffac4de0eac43f8ad0b
lby2013 <1137992402@qq.com>
2018-08-18 21:09:57 +0800

    为检索模型预留接口

--- test_port2.py
+++ test_port2.py
@@ -1,9 +1,12 @@
 from chatbot_cut.test_port import  chatbot_port
 import sys
 import os
+#from tfidf.test_port import chatbot_port2
 import time
 s=chatbot_port()
+#t=chatbot_port2()
 print("?????")
+type=0
 while(True):
     st=''
     with open('qq_intput.txt', 'r', encoding='utf-8') as f:
@@ -12,7 +15,11 @@ while(True):
     time.sleep(0.1)
     if len(st)>=1 and st[0]!='%':
         print('2')
-        st=s.chat(st[1:])
+        if type==0:
+            st=s.chat(st[1:])
+        else:
+            st=t.chat(st[1:])
         with open('qq_intput.txt', 'w', encoding='utf-8') as f:
             print('%',st[:st.find('<')],file=f)
             print('3')
+
---
+++ tfidf/test_port.py
@@ -0,0 +1,13 @@
+#import ???
+sys.path.append('..')
+
+
+
+class chatbot_port(object):
+    def __init__(self):
+
+        print("!!!!")
+    def chat(self,user_text):
+        st=user_text
+        return st
+
